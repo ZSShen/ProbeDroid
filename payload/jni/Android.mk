55ff528f7f37091ef04f619feaecd1f4a862363c
ZSShen <andy780328@hotmail.com>
2015-12-27 18:07:33 +0800

    Update the hooking payload for new batch construction.

--- payload/jni/Android.mk
+++ payload/jni/Android.mk
@@ -5,18 +5,23 @@ include $(CLEAR_VARS)
 LOCAL_MODULE            := hook
 
 LOCAL_C_INCLUDES        +=  ../../common \
+                            ../../android/art/runtime \
+                            ./gadget
 
 LOCAL_SRC_FILES         :=  ../../common/util.cc \
-                            trampoline.s \
+                            ../../common/utf.cc \
+                            ../../android/art/runtime/mirror/string.cc \
+                            ./gadget/java/lang/stringbuilder-asm.s \
+                            ./gadget/java/lang/stringbuilder.cc \
+                            ./gadget/gadget-asm.s \
                             hook.cc \
 
-LOCAL_CFLAGS            := -g -fexceptions -fpermissive
+LOCAL_CFLAGS            := -g -fexceptions
 
 LOCAL_SHARED_LIBRARIES  := dl
 
 LOCAL_LDLIBS            +=  -L$(SYSROOT)/usr/lib \
                             -llog \
-                            -latomic
 
 LOCAL_CPP_EXTENSION     := .cxx .cpp .cc
 
--- payload/jni/hook.cc
+++ payload/jni/hook.cc
@@ -1,46 +1,25 @@
-#include <util.h>
-#include <bridge.h>
 
-#define LOG_TAG "DEBUG"
-#define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, fmt, ##args)
+#include "util.h"
+#include "logcat.h"
+#include "mirror/string-inl.h"
+#include "mirror/array-inl.h"
+#include "gadget.h"
+#include "java/lang/stringbuilder.h"
+
 
 using namespace util;
+using namespace art;
 
-
-// The original functions.
-FUNC_INVOKE_VIRTUAL InvokeVirtualOriginal;
-FUNC_RESOLVE_STRING ResolveStringOriginal;
-FUNC_VIRTUAL_METHOD_CALL VirtualMethodCallOriginal;
-
-
-void __attribute__((constructor)) entry()
+void __attribute__((constructor)) HookEntry()
 {
     LOGD("\n\nHook success, pid = %d\n\n", getpid());
 
     JNIEnv *env;
-    GetJNIENVExt(&env);
+    GetJniEnv(&env);
 
-    jclass clazz = env->FindClass("java/lang/Object");
-    jmethodID method = env->GetMethodID(clazz, "getClass", "()Ljava/lang/Class;");
-
-    uint32_t addr = (uint32_t)method;
-    FUNC_VIRTUAL_METHOD_CALL* addr_quick_entry =
-                        (FUNC_VIRTUAL_METHOD_CALL*)(addr + 40);
-    VirtualMethodCallOriginal = *addr_quick_entry;
-    *addr_quick_entry = VirtualMethodCallFake;
+    // TODO: Currently, only a simple gadget for StringBuilder.toString() is
+    // is applied. In the long tern, we should design a gadget compiler to build
+    // the gadgets for multiple hook points.
+    Gadget_java_lang_StringBuilder gadget;
+    bool result = gadget.toStringPatcher(env);
 }
-
-
-void ResolveStringProfiling(void* referrer, uint32_t string_idx)
-{
-    // Now it is just a dummy hook to verify our successful hook.
-    LOGD("\n[+] Hooked ResolveString(): ArtMethod=0x%08x StringId=0x%08x\n",
-         referrer, string_idx);
-}
-
-
-void VirtualMethodCallProfiling()
-{
-    LOGD("\n[+] Execute java.lang.Object.getClass\n");
-}
-
