6346df12fe221c93a79f8bd058bc01fd38f00500
ZSShen <andy780328@hotmail.com>
2016-03-04 17:26:30 +0800

    The first analysis apk wrapped with the Instrument SDK which can help to debug.

---
+++ tools/HelloWorld/app/src/main/java/org/zsshen/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.zsshen.helloworld;
+
+
+import org.probedroid.*;
+
+
+public class HelloWorld extends Instrument {
+
+    @Override
+    public void onApplicationStart() {
+
+    }
+
+    @Override
+    public void onApplicationStop() {
+
+    }
+}
