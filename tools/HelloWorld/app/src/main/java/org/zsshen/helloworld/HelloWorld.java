1dfffb194215f76c2a4264359249b16ab384981e
ZSShen <andy780328@hotmail.com>
2016-03-25 20:33:20 +0800

    Remove the temporary debugging package.

--- tools/HelloWorld/app/src/main/java/org/zsshen/helloworld/HelloWorld.java
+++ tools/HelloWorld/app/src/main/java/org/zsshen/helloworld/HelloWorld.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import org.probedroid.Instrument;
+import org.zsshen.helloworld.bundle.ProbeActivity;
 import org.zsshen.helloworld.bundle.StringBuilderToString;
 
 import android.util.Log;
@@ -18,13 +19,13 @@ public class HelloWorld extends Instrument {
     @Override
     public void onApplicationStart() {
         // Instrument "String StringBuilder.toString()".
-        // String nameClass = "java.lang.StringBuffer";
-        // String nameMethod = "append";
-        // String signatureMethod =
-        // "(Ljava/lang/String;)Ljava/lang/StringBuffer;";
-        String nameClass = "java.lang.StringBuilder";
-        String nameMethod = "toString";
-        String signatureMethod = "()Ljava/lang/String;";
+        //String nameClass = "java.lang.StringBuilder";
+        //String nameMethod = "toString";
+        //String signatureMethod = "()Ljava/lang/String;";
+        /*
+        String nameClass = "java.lang.StringBuffer";
+        String nameMethod = "append";
+        String signatureMethod = "(Ljava/lang/String;)Ljava/lang/StringBuffer;";
 
         StringBuilderToString bundleSBToString = new StringBuilderToString(true,
                 true);
@@ -36,24 +37,35 @@ public class HelloWorld extends Instrument {
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
         }
-
+        */
+        String nameClass = "android.app.Activity";
+        String nameMethod = "startActivity";
+        String signatureMethod = "(Landroid/content/Intent;Landroid/os/Bundle;)V";
+        //android.os.Bundle
+        //String nameClass = "org.zsshen.hookvictim.MainActivity";
+        //String nameMethod = "TestVoid";
+        //String signatureMethod = "()V";
+        ProbeActivity probe = new ProbeActivity(true, true);
+        try {
+            instrumentMethod(false, nameClass, nameMethod, signatureMethod, probe);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         gListString = new ArrayList<String>();
     }
 
     @Override
     public void onApplicationStop() {
         Log.d("HelloWorld", "OK Terminate now");
-
         StringBuffer sb = new StringBuffer();
         for (String str : gListString) {
             sb.append(str);
             sb.append("\n");
         }
-
-        //Log.d("HelloWorld", sb.toString());
-        File file = new File("./InstrumentResult.txt");
-        String path = file.getAbsolutePath();
-        Log.d("HelloWorld", path);
+        File file = new File(
+                "/data/data/com.kkbox.kktix/InstrumentResult.txt");
         BufferedWriter output = null;
         try {
             output = new BufferedWriter(new FileWriter(file));
